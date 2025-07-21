ecf9aca3c7b260e06ba3de5c98e05e4d32a0c3a7
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-21 12:44:35 +0900

    config load

--- pattern_2_with-python/main.py
+++ pattern_2_with-python/main.py
@@ -32,6 +32,54 @@ config = {
         },
     },
 }
+
+# 1. load the /etc/discord/login-notice.json
+# 2. load the ~/.python/login-notice.json and override
+# 3. load the ./login-notice.json and override
+config_file = 'login-notice.json'
+config_loadstate = 16 # 0b10000
+# 0b10000
+#   ^^^^^
+#   |||||
+#   ||||+-  1) エラー扱い
+#   |||+--  2) Load succeede ./login-notice.json
+#   ||+---  4) Load succeede ~/.python/login-notice.json
+#   |+----  8) Load succeede /etc/discord/login-notice.json
+#   +----- 16) 桁揃え用（非使用）
+config_dir = '/etc/discord/'
+if os.path.exists(config_dir+config_file):
+    try:
+        with open(config_dir+config_file,mode='r',encoding='utf-8') as f:
+            config|=json.load(f)
+            config_loadstate|=8
+    except (FileNotFoundError, PermissionError, json.JSONDecodeError, UnicodeDecodeError, NameError, TypeError):
+        pass
+config_dir = '~/.python/'
+if os.path.exists(config_dir+config_file):
+    try:
+        with open(config_dir+config_file,mode='r',encoding='utf-8') as f:
+            config|=json.load(f)
+            config_loadstate|=4
+    except (FileNotFoundError, PermissionError, json.JSONDecodeError, UnicodeDecodeError, NameError, TypeError):
+        pass
+config_dir = './'
+if os.path.exists(config_dir+config_file):
+    try:
+        with open(config_dir+config_file,mode='r',encoding='utf-8') as f:
+            config|=json.load(f)
+            config_loadstate|=2
+    except (FileNotFoundError, PermissionError, json.JSONDecodeError, UnicodeDecodeError, NameError, TypeError):
+        pass
+try:
+    config['runner']['config']['loadstate'] = config_loadstate
+except (ValueError, KeyError):
+    pass
+if config_loadstate^16==0:
+    config_loadstate = 1
+    print('[Config] No such file or directory: (global, user, temporary)')
+    print(f'[{__name__}] Exiting...')
+    sys.exit(config_loadstate)
+
 try:
     config['env']['tmout'] = int(config['env']['tmout'])
 except (ValueError, KeyError):
