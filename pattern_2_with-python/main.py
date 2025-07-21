1fa1ee8f74a162db929776da3cba2824df67d369
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-21 15:59:11 +0900

    discord_field_json

--- pattern_2_with-python/main.py
+++ pattern_2_with-python/main.py
@@ -211,4 +211,10 @@ discord_field_json = {
     'inline': False,
 }
 discord_embed_json['fields'].append(discord_field_json)
+discord_field_json = {
+    'name': '> From',
+    'value': f'''[{config['env']['os_dependent']['linux']['common']['ssh_client'][0]}:{config['env']['os_dependent']['linux']['common']['ssh_client'][1]}](https://ipinfo.io/{config['env']['os_dependent']['linux']['common']['ssh_client'][0]})''',
+    'inline': False,
+}
+discord_embed_json['fields'].append(discord_field_json)
 print(json.dumps(config,indent=4))
