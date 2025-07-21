b047417d89131a525e9646045a5e58d7ad048f43
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-21 11:37:45 +0900

    config.discord

--- pattern_2_with-python/main.py
+++ pattern_2_with-python/main.py
@@ -5,6 +5,15 @@ import json
 import requests
 
 config = {
+    'discord': {
+        'webhook':{
+            'url': 'https://discord.com/api/webhooks/',
+        },
+        'avatar':{
+            'url': 'https://hangstuck.com/wp-content/uploads/2020/08/bash-official-icon-512x512-1.png',
+            'name': sub('\.*', '', os.environ.get('HOSTNAME')),
+        },
+    },
 }
 
 print(json.dumps(config,indent=4))
