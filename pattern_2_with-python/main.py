dae5a3bc0217708324b9b964b5f1b18b89806ec8
Komiya Yu <komiya.yu@n138.jp>
2025-10-19 02:33:57 +0900

    ./login-notice.dat

--- pattern_2_with-python/main.py
+++ pattern_2_with-python/main.py
@@ -398,8 +398,11 @@ def main():
             json.dump(config, f, indent=4)
         print(f'[{__name__}] Exiting...')
         sys.exit(config_loadstate)
-    with open('./login-notice.dat',mode='w',encoding='utf-8') as f:
-        json.dump(config, f, indent=4)
+    try:
+        with open('./login-notice.dat',mode='w',encoding='utf-8') as f:
+            json.dump(config, f, indent=4)
+    except (FileNotFoundError, PermissionError):
+        pass
 
     try:
         config['env']['tmout'] = int(config['env']['tmout'])
